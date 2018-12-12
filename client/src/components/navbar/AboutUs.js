825c2e3af59babca032ee76bf120ea3ec246816c
Jordan Dale <jordan.dale2@gmail.com>
2018-12-12 12:34:42 -0500

    Added NavBar to Homepage

---
+++ client/src/components/navbar/AboutUs.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AboutUs extends Component {
+    render() {
+        return (
+            <div>
+                Hello from About Us component
+            </div>
+        );
+    }
+}
+
+export default AboutUs;
---
+++ client/src/components/navbar/HowItWorks.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HowItWorks extends Component {
+    render() {
+        return (
+            <div>
+                Hello from How It Works component
+            </div>
+        );
+    }
+}
+
+export default HowItWorks;
