8234896031a7e2865cbe6c1a7d808ec46cc44ef0
Loren Kelley <kelley.loren52@gmail.com>
2018-12-13 08:46:12 -0500

    created components

---
+++ client/src/components/UserAccountPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class UserAccountPage extends Component {
+    render() {
+        return (
+            <div>
+                <h2>USER ACCOUNT</h2>
+            </div>
+        );
+    }
+}
+
+export default UserAccountPage;
---
+++ client/src/components/UserNavMenu.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class UserNavMenu extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default UserNavMenu;
