76c4e3c2dd74c5f77e501e018774c739121e269d
Loren Kelley <kelley.loren52@gmail.com>
2018-12-15 14:54:21 -0500

    added view quote button

--- client/src/components/UserDashboard.js
+++ client/src/components/UserDashboard.js
@@ -19,7 +19,9 @@ margin-top: 80px;
 font-size: 25px;
 `
 const Job = styled.div `
+font-size: 20px;
 margin-top: 20px;
+text-align: left;
 border: solid 1px rgba(112,112,112,1);
 width: 400px;
 height: 50px;
@@ -28,7 +30,6 @@ background: rgba(255,255,255,1);
 border-radius: 5px 5px 5px 5px;
 color: rgba(66,66,66,1);
 `
-
 
 
 class UserDashboard extends Component {
