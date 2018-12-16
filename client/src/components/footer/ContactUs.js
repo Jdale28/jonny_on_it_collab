20528c9fa2c318563c0ff21d95579d3e7ff0c51e
chuckderosier <chuckderosier2018@gmail.com>
2018-12-16 12:34:09 -0500

    finished contact us style

--- client/src/components/footer/ContactUs.js
+++ client/src/components/footer/ContactUs.js
@@ -3,43 +3,70 @@ import styled from 'styled-components'
 import { BlueButton } from '../ButtonStyle'
 import { FormStyled } from '../FormStyle'
 
-const ContactStyle = styled.div`
+const SplitPage = styled.div`
+    display: flex;
+`
 
+const ContactStyle = styled.div`
+    width: 40vw;
+    margin: 3em auto;
+    border-left: solid 1px rgba(0,0,0,1);
+    display: flex;
+    flex-direction: column;
+    div {
+        margin: 0 0 0 6em;
+    }
+    h5 {
+        color: rgba(14,151,255,1);
+        margin: 1em 0 0 0;
+    }
+    p {
+        margin: 0;
+    }
 `
 
 class ContactUs extends Component {
     render() {
         return (
-            <FormStyled>
-                <h2>Contact Us</h2>
-                <div className="form-container">
-                    <form>
-                        <p>First Name<span className="span">*</span><span className="span" className="right">* Required</span></p>
-                        <input type="text" />
-                        <p>Last Name<span className="span">*</span></p>
-                        <input type="text" />
-                        <p>Email<span className="span">*</span></p>
-                        <input type="text" />
-                        <div className="text-area">
-                            <p>Details<span className="span">*</span></p>
-                            <textarea type="text" columns="45" rows="8" />
-                        </div>
-                        <div className="submit-button">
-                            <BlueButton type="submit">Contact Us</BlueButton>
-                        </div>
-                    </form>
-                </div>
+            <SplitPage>
+                <FormStyled>
+                    <h2>Contact Us</h2>
+                    <div className="form-container">
+                        <form>
+                            <p>First Name<span className="span">*</span><span className="span" className="right">* Required</span></p>
+                            <input type="text" />
+                            <p>Last Name<span className="span">*</span></p>
+                            <input type="text" />
+                            <p>Email<span className="span">*</span></p>
+                            <input type="text" />
+                            <div className="text-area">
+                                <p>Details<span className="span">*</span></p>
+                                <textarea type="text" columns="45" rows="8" />
+                            </div>
+                            <div className="submit-button">
+                                <BlueButton type="submit">Contact Us</BlueButton>
+                            </div>
+                        </form>
+                    </div>
+                </FormStyled>
                 <ContactStyle>
-                    <h4>Adress</h4>
-                    <p>3423 Piedmont Road NE</p>
-                    <p>Atlanta, Georgia 30305</p>
-                    <h4>Phone</h4>
-                    <p>(833) Hi Jonny</p>
-                    <p>(833) 445 - 6669</p>
-                    <h4>Email</h4>
-                    <p>info@jonnyonit.com</p>
+                    <div>
+                        <h5>Address</h5>
+                        <p>3423 Piedmont Road NE</p>
+                        <p>Atlanta, Georgia 30305</p>
+                    </div>
+                    <div>
+                        <h5>Phone</h5>
+                        <p>(833) Hi Jonny</p>
+                        <p>(833) 445 - 6669</p>
+                    </div>
+                    <div>
+                        <h5>Email</h5>
+                        <p>info@jonnyonit.com</p>
+                    </div>
                 </ContactStyle>
-            </FormStyled>
+
+            </SplitPage>
         )
     }
 }
